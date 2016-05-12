4cfff2844558fd0aac0187cce1abbcaf64f8da50
Abdirahman <abdirahman@Abdis-MacBook-Pro.local>
2016-05-12 03:48:14 +0200

    Model

--- MovieList/app/src/main/java/com/example/abdirahman/movielist/DALC/DAO.java
+++ MovieList/app/src/main/java/com/example/abdirahman/movielist/DALC/DAO.java
@@ -1,7 +1,92 @@
 package com.example.abdirahman.movielist.DALC;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by abdirahman on 26/04/16.
  */
 public class DAO {
+    SQLiteDatabase db;
+    private static DAO instance;
+
+    private DAO(Context context) {
+        db = new DB(context).getWritableDatabase();
+    }
+
+    public static DAO getInstance() {
+        return instance;
+    }
+
+    public static void createInstance(Context context){
+        instance = new DAO(context);
+    }
+
+    public long addMovie(Movie movie) {
+        ContentValues values = new ContentValues();
+        values.put(DB.ID, movie.getId());
+        values.put(DB.TITLE, movie.getTitle());
+        values.put(DB.RELEASE, movie.getRelease());
+        values.put(DB.IMAGEURL, movie.getImageURL());
+        values.put(DB.OVERVIEW, movie.getOverview());
+
+        if (movie.isStored()) {
+            values.put(DB.STORED, 1);
+        } else {
+            values.put(DB.STORED, 0);
+        }
+        return db.insert(DB.TABLE_NAME, null, values);
+    }
+
+    public int deleteMovie(long id) {
+        return db.delete(DB.TABLE_NAME, DB.ID_EQUAL, DB.stringArray(id));
+    }
+
+    public int deleteMovies() {
+        return db.delete(DB.TABLE_NAME, null, null);
+    }
+
+    public int updateMovie(long id, Movie movie) {
+        ContentValues values = new ContentValues();
+        values.put(DB.TITLE, movie.getTitle());
+        values.put(DB.RELEASE, movie.getRelease());
+        values.put(DB.IMAGEURL, movie.getImageURL());
+        values.put(DB.OVERVIEW, movie.getOverview());
+        if (movie.isStored()) {
+            values.put(DB.STORED, 1);
+        } else {
+            values.put(DB.STORED, 0);
+        }
+        return db.update(DB.TABLE_NAME, values, DB.ID_EQUAL, DB.stringArray(id));
+    }
+
+    // returens a cusor contaning
+    public Cursor getMovies() {
+        return db.query(DB.TABLE_NAME, DB.ALL_COLUMNS, null, null, null, null,
+                null);
+    }
+
+    public Movie getMovie(long id) {
+        Cursor c = db.query(DB.TABLE_NAME, DB.ALL_COLUMNS, DB.ID_EQUAL,
+                DB.stringArray(id), null, null, null);
+        if (c.moveToFirst()) {
+            int Id = c.getInt(c.getColumnIndex(DB.ID));
+            String title = c.getString(c.getColumnIndexOrThrow(DB.TITLE));
+            String release = c.getString(c
+                    .getColumnIndexOrThrow(DB.RELEASE));
+            String imageurl = c.getString(c.getColumnIndexOrThrow(DB.IMAGEURL));
+            String overview = c.getString(c.getColumnIndexOrThrow(DB.OVERVIEW));
+            boolean stored = c.getInt(c.getColumnIndex(DB.STORED)) > 0;
+            return new Movie(Id, title, release, overview, imageurl, stored);
+        } else
+            return null;
+    }
+
+
+    public void close() {
+        db.close();
+    }
 }
+
--- MovieList/app/src/main/java/com/example/abdirahman/movielist/DALC/DB.java
+++ MovieList/app/src/main/java/com/example/abdirahman/movielist/DALC/DB.java
@@ -1,7 +1,49 @@
 package com.example.abdirahman.movielist.DALC;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by abdirahman on 26/04/16.
  */
-public class DB {
+public class DB extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 3;
+    private static final String DATABASE_NAME = "movies.db";
+
+    public static final String TABLE_NAME = "movies";
+    // for cursor adapter the table (or the cursor) must include a column named "_id"
+    public static final String ID = "_id";
+    public static final String TITLE = "title";
+    public static final String RELEASE = "release";
+    public static final String IMAGEURL = "imageurl";
+    public static final String OVERVIEW = "overview";
+    public static final String STORED = "stored";
+
+    public static final String[] ALL_COLUMNS = new String[] {ID, TITLE, RELEASE, IMAGEURL, OVERVIEW, STORED};
+    public static final String[] LIST_COLUMNS = new String[] {TITLE, RELEASE, IMAGEURL, OVERVIEW, STORED};
+    public static final String ID_EQUAL = ID + "=?";
+
+    public static String[] stringArray(long id) {
+        return new String[] {Long.toString(id)};
+    }
+    // SQL create table statement
+    private static final String CREATE_TABLE = "create table " + TABLE_NAME + " (" + ID
+            + " integer primary key, " + TITLE + " text not null, " + RELEASE
+            + " text not null, " + IMAGEURL + " text, " + OVERVIEW + " text, " + STORED + " integer not null" +");";
+
+    DB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
 }
